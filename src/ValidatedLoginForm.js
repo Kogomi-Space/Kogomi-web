039aa563fcba865477833d0dfd2525b1ddd778e2
dain98 <dain98@gmail.com>
2020-04-13 16:29:22 +0900

    Changed Login Form UI to use Google Material UI

--- src/ValidatedLoginForm.js
+++ src/ValidatedLoginForm.js
@@ -1,76 +1,187 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import checkLogin from './CheckLogin'
 
-const ValidatedLoginForm = (props, status) => (
-    <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-                setSubmitting(false);
-                // loggedIn = true;
-                if (checkLogin(values.email,values.password)) {
-                    props.history.push('/main');
-                }
-            }, 500);
-        }}
-        validationSchema={Yup.object().shape({
-            email: Yup.string()
-                .email()
-                .required("Required"),
-            password: Yup.string()
-                .required("No password provided.")
-                .min(8, "Password is too short - should be 8 characters minimum.")
-                .matches(/(?=.*[0-9])/, "Password must contain a number.")
-        })}
-    >
-        { props => {
-            const {
-                values,
-                touched,
-                errors,
-                isSubmitting,
-                handleChange,
-                handleBlur,
-                handleSubmit
-            } = props;
-            return (
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        name="email"
-                        type="text"
-                        placeholder="Enter your email"
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className={errors.email && touched.email && "error"}
-                    />
-                    {errors.email && touched.email && (
-                        <div className="input-feedback">{errors.email}</div>
-                    )}
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
-                    <label htmlFor="email">Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className={errors.password && touched.password && "error"}
-                    />
-                    {errors.password && touched.password && (
-                        <div className="input-feedback">{errors.password}</div>
-                    )}
-                    <button type="submit" disabled={isSubmitting} >
-                        Login
-                    </button>
-                </form>
-            );
-        }}
-    </Formik>
-);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: 'url(http://zenith.blue/i/t2vfh.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const ValidatedLoginForm = (props, status) => {
+    console.log('props',props)
+    const classes = useStyles();
+    return (
+        <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    setSubmitting(false);
+                    // loggedIn = true;
+                    if (checkLogin(values.email,values.password)) {
+                        props.history.push('/main');
+                    }
+                }, 500);
+            }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email()
+                    .required("Required"),
+                password: Yup.string()
+                    .required("No password provided.")
+                    .min(8, "Password is too short - should be 8 characters minimum.")
+                    .matches(/(?=.*[0-9])/, "Password must contain a number.")
+            })}
+        >
+            { props => {
+                const {
+                    values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit
+                } = props;
+                return (
+                    <Grid container component="main" className={classes.root} >
+                        <CssBaseline />
+                        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+                        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                            <div className={classes.paper}>
+                                <Avatar className={classes.avatar}>
+                                    <LockOutlinedIcon />
+                                </Avatar>
+                                <Typography component="h1" variant="h5">
+                                    Sign in
+                                </Typography>
+                                <form className={classes.form} onSubmit={handleSubmit}>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocus
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.email && touched.email && "error"}
+                                    />
+                                    {errors.email && touched.email && (
+                                        <div className="input-feedback">{errors.email}</div>
+                                    )}
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.password && touched.password && "error"}
+                                    />
+                                    {errors.password && touched.password && (
+                                        <div className="input-feedback">{errors.password}</div>
+                                    )}
+                                    <FormControlLabel
+                                        control={<Checkbox value="remember" color="primary" />}
+                                        label="Remember me"
+                                    />
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                        className={classes.submit}
+                                    >
+                                        Sign In
+                                    </Button>
+                                    <Grid container>
+                                        <Grid item xs>
+                                            <Link href="#" variant="body2">
+                                                Forgot password?
+                                            </Link>
+                                        </Grid>
+                                        <Grid item>
+                                            <Link href="#" variant="body2">
+                                                {"Don't have an account? Sign Up"}
+                                            </Link>
+                                        </Grid>
+                                    </Grid>
+                                    <Box mt={5}>
+                                        <Copyright />
+                                    </Box>
+                                </form>
+                            </div>
+                        </Grid>
+                    </Grid>
+                );
+            }}
+        </Formik>
+    );
+}
+
 
 export default ValidatedLoginForm;
