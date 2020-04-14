d468b9a56792c2265592c7c366cede31a8e8180c
dain98 <dain98@gmail.com>
2020-04-14 18:16:20 +0900

    Developing Dashboard

--- src/Main/menuItems.js
+++ src/Main/menuItems.js
@@ -1,43 +1,41 @@
-{
-  "data": [
+export const menu = [
     {
-      "name": "Dashboard",
-      "url": "",
-      "icon": "DashboardIcon"
+      name: "Dashboard",
+      url: "",
+      icon: "DashboardIcon"
     },
     {
-      "name": "User Settings",
-      "url": "usersettings",
-      "icon": "UserSettingsIcon",
-      "children": [
+      name: "User Settings",
+      url: "usersettings",
+      icon: "UserSettingsIcon",
+      children: [
         {
-          "name": "Sample 1",
-          "url": "usersettings/sample1",
-          "icon": "SampleIcon"
+          name: "Sample 1",
+          url: "usersettings/sample1",
+          icon: "SampleIcon"
         },
         {
-          "name": "Sample 2",
-          "url": "usersettings/sample2",
-          "icon": "SampleIcon"
+          name: "Sample 2",
+          url: "usersettings/sample2",
+          icon: "SampleIcon"
         }
       ]
     },
     {
-      "name": "Discord Settings",
-      "url": "discordsettings",
-      "icon": "DiscordIcon",
-      "children": [
+      name: "Discord Settings",
+      url: "discordsettings",
+      icon: "DiscordIcon",
+      children: [
         {
-          "name": "osu!",
-          "url": "/osu",
-          "icon": "OsuIcon"
+          name: "osu!",
+          url: "/osu",
+          icon: "OsuIcon"
         },
         {
-          "name": "League of Legends",
-          "url": "LoL",
-          "icon": "LeagueIcon"
+          name: "League of Legends",
+          url: "LoL",
+          icon: "LeagueIcon"
         }
       ]
     }
-  ]
-}
+]
