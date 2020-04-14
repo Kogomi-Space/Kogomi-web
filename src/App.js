5e8961993c6b4271094917d706aac27c348f0b7f
dain98 <dain98@gmail.com>
2020-04-14 18:15:18 +0900

    Playing around with Router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 // import { Connect } from 'react-redux';
 
 import ValidatedLoginForm from "./ValidatedLoginForm";
-import Main from "./Main";
+import Main from "./Main/Main";
 import Home from "./Home";
+import Test from "./Main/Test";
+import NotFound from "./Pages/NotFound";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            authed: true,
+            authed: false,
             loggedIn: true
         };
     }
@@ -21,9 +23,13 @@ class App extends React.Component {
             <div>
                 <Switch>
                     <Route exact path="/" render={() => <Home authed={this.state.authed} />} />
-                    <Route exact path="/main" render={(props) => <Main {...props} loggedIn={this.state.loggedIn} />} />
+                    <Route exact path="/main" component={Main} />
+                    {/*    <Route exact path="discordsettings" >*/}
+                    {/*        <Route exact path="osu" component={Test}/>*/}
+                    {/*    </Route>*/}
+                    {/*</Route>*/}
                     <Route exact path="/login" component={ValidatedLoginForm} />
-                    <Route path="/:id" render = {()=> (<p>404 - Page Not Found</p>)}/>
+                    <NotFound />
                 </Switch>
             </div>
         );
