b8638d84c24ef387c0b079a110bf711bed425414
dain98 <dain98@gmail.com>
2020-04-28 17:18:15 +0900

    Updates

---
+++ precache-manifest.e197e747bdcae585b75c6cc98f6a976b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf2d4e30aba344388a158fdbc474d92",
+    "url": "/cubchoo-web/index.html"
+  },
+  {
+    "revision": "2944b78047235b26f4c9",
+    "url": "/cubchoo-web/static/css/main.bef0fa75.chunk.css"
+  },
+  {
+    "revision": "218fe70a5e0c4a51c7e6",
+    "url": "/cubchoo-web/static/js/2.567a0c00.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/cubchoo-web/static/js/2.567a0c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2944b78047235b26f4c9",
+    "url": "/cubchoo-web/static/js/main.ebeb80fe.chunk.js"
+  },
+  {
+    "revision": "313eb2fd8feb1e488f40",
+    "url": "/cubchoo-web/static/js/runtime-main.6de8bada.js"
+  }
+]);
